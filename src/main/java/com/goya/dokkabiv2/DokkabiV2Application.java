393d3fe76844e395f834c8c5de5845b9f35d80d0
JinhaJjing <tjwlsgkkgslwjt@gmail.com>
2024-10-22 11:06:09 +0900

    Initial commit

---
+++ src/main/java/com/goya/dokkabiv2/DokkabiV2Application.java
@@ -0,0 +1,13 @@
+package com.goya.dokkabiv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DokkabiV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DokkabiV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/goya/dokkabiv2/DokkabiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goya.dokkabiv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DokkabiV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
