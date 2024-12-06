d85eae054061d688d0a8f126851c2878599b1e8e
JinhaJjing <tjwlsgkkgslwjt@gmail.com>
2024-12-06 16:58:26 +0900

    CORS 필터 추가, HTTPS 강제 로직 제거

--- src/main/java/com/goya/dokkabiv2/util/SecurityConfig.java
+++ src/main/java/com/goya/dokkabiv2/util/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
@@ -21,7 +22,7 @@ import java.util.Arrays;
 public class SecurityConfig {
 
     @Bean
-    public CorsFilter corsFilter() {
+    public CorsConfigurationSource corsConfigurationSource() {
         // CORS 설정
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.addAllowedOrigin("*"); // 허용할 Origin
@@ -29,11 +30,10 @@ public class SecurityConfig {
         corsConfiguration.addAllowedHeader("*"); // 허용할 헤더
         corsConfiguration.setAllowCredentials(true); // 자격 증명 허용
 
-        // CORS 정책을 경로에 적용
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfiguration);
+        source.registerCorsConfiguration("/**", corsConfiguration); // 모든 경로에 대해 CORS 설정 적용
 
-        return new CorsFilter(source);  // CORS 필터를 반환
+        return source; // CORS 설정을 반환
     }
 
     @Bean
@@ -44,9 +44,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
         http
-                .requiresChannel(channel ->
-                        channel.anyRequest().requiresSecure() // 모든 요청을 HTTPS로 강제
-                )
+                .addFilterBefore(new CorsFilter(corsConfigurationSource()), UsernamePasswordAuthenticationFilter.class) // CORS 필터를 가장 먼저 처리
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api-docs/**").permitAll() // Swagger 접근 허용
                         .requestMatchers("/test").permitAll() // Swagger 접근 허용
@@ -54,10 +52,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated() // 다른 요청은 인증 필요
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class) // JWT 필터 추가
-                .oauth2Login(oauth2 -> oauth2
-                        //.loginPage("/login") // 사용자 정의 로그인 페이지
-                        .defaultSuccessUrl("/test-schema", true) // 로그인 성공 후 리디렉션될 URL
-                );
+        ;
 
         return http.build();
     }
